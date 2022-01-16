008dad9fcf02a19ac048dc2f69994e24befa219c
Zaur Malakhov <1zaur1@mail.ru>
2022-01-16 06:18:41 +0300

    8-1-2 Добавит модуль ApiService в точке входа. WIP

--- src/main.js
+++ src/main.js
@@ -6,12 +6,16 @@ import MainPresenter from './presenter/main-presenter.js';
 import FilmsModel from './model/films-model.js';
 import FilterModel from './model/filter-model.js';
 import FilterPresenter from './presenter/filter-presenter.js';
+import ApiService from './api-service.js';
+
+const AUTHORIZATION = 'Basic a67bfj56nkldfvm5k';
+const END_POINT = 'https://16.ecmascript.pages.academy/cinemaddict';
 
 const API = new APIMOCK();
 const films = API.getFilms();
 // const films = [];
 
-const filmsModel = new FilmsModel();
+const filmsModel = new FilmsModel(new ApiService(END_POINT, AUTHORIZATION));
 filmsModel.films = films;
 
 const filterModel = new FilterModel();
--- src/model/films-model.js
+++ src/model/films-model.js
@@ -1,7 +1,21 @@
 import AbstractObservable from '../utils/abstract-observable.js';
 
 export default class FilmsModel extends AbstractObservable {
+  #apiService = null;
   #films = [];
+
+  constructor(apiService) {
+    super();
+    this.#apiService = apiService;
+
+    this.#apiService.films.then((films) => {
+      console.log(films);
+      // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
+      // а ещё на сервере используется snake_case, а у нас camelCase.
+      // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
+      // Есть вариант получше - паттерн "Адаптер"
+    });
+  }
 
   set films(films) {
     this.#films = [...films];
