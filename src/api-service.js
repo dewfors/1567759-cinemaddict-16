f5c10a705543e7e572b449094c1746a1a047b702
Zaur Malakhov <1zaur1@mail.ru>
2022-01-16 05:51:20 +0300

    8-1-1 Создаст класс для работы с API

---
+++ src/api-service.js
@@ -0,0 +1,65 @@
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+};
+
+export default class ApiService {
+  #endPoint = null;
+  #authorization = null;
+
+  constructor(endPoint, authorization) {
+    this.#endPoint = endPoint;
+    this.#authorization = authorization;
+  }
+
+  get films() {
+    return this.#load({url: 'movies'})
+      .then(ApiService.parseResponse);
+  }
+
+  updateFilm = async (film) => {
+    const response = await this.#load({
+      url: `movies/${film.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(film),
+      headers: new Headers({'Content-Type': 'application/json'}),
+    });
+
+    const parsedResponse = await ApiService.parseResponse(response);
+
+    return parsedResponse;
+  }
+
+  #load = async ({
+    url,
+    method = Method.GET,
+    body = null,
+    headers = new Headers(),
+  }) => {
+    headers.append('Authorization', this.#authorization);
+
+    const response = await fetch(
+      `${this.#endPoint}/${url}`,
+      {method, body, headers},
+    );
+
+    try {
+      ApiService.checkStatus(response);
+      return response;
+    } catch (err) {
+      ApiService.catchError(err);
+    }
+  }
+
+  static parseResponse = (response) => response.json();
+
+  static checkStatus = (response) => {
+    if (!response.ok) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+  }
+
+  static catchError = (err) => {
+    throw err;
+  }
+}
