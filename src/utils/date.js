6c926b10e7d36eeeb906a9e2557174f1928aaf08
Zaur Malakhov <1zaur1@mail.ru>
2022-01-18 18:36:54 +0300

    8-3-1 Реализует вывод даты публикации комментария в человеческом формате

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,10 +1,12 @@
 import dayjs from 'dayjs';
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 import isBefore from 'dayjs/plugin/isSameOrBefore';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import {MINUTES_IN_HOUR} from './const.js';
 
 dayjs.extend(isBefore);
 dayjs.extend(isSameOrBefore);
+dayjs.extend(relativeTime);
 
 export const isDateInRange = (currentDate, dateFrom) => dayjs(dateFrom).isSameOrBefore(currentDate);
 
@@ -18,4 +20,8 @@ export const humanizeDuration = (duration, {asObject = false} = {}) => {
     };
   }
   return `${hours}h ${minutes}m`;
+};
+
+export const humanizeTimeFromNow = (date) => {
+  return dayjs(date).fromNow();
 };
